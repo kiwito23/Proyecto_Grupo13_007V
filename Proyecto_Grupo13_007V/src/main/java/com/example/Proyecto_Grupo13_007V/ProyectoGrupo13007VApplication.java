78f9623238d9e07b9fa95f426e0798160b4d653d
kiwi1004 <ka.olmedo@duocuc.cl>
2026-04-30 20:31:50 -0400

    first commit

---
+++ Proyecto_Grupo13_007V/src/main/java/com/example/Proyecto_Grupo13_007V/ProyectoGrupo13007VApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Grupo13_007V;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo13007VApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupo13007VApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Grupo13_007V/src/test/java/com/example/Proyecto_Grupo13_007V/ProyectoGrupo13007VApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Grupo13_007V;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo13007VApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
